f6beb595a49a963b0851601b59db865cc35b9f2e
bhimatak <bhima.t@gmail.com>
2024-02-07 19:16:22 +0530

    updated

--- Prgs/dec1.py
+++ Prgs/dec1.py
@@ -1,18 +1,39 @@
-def outter_fun():
+def outter_fun(func):
     print("Dec 1")
-    def inner_func():
+    def inner_func(str1):
         print("inner function")
 
-        # print()
-        return "hello world"
+        print(str1)
+        ret = func(str1)
+        print("inside inner_function: ",ret)
+        ret = list(ret)
+        return ret
     print("Exiting from outer function")
+    # func()
     return inner_func
 
-def f1():
-    return "hello1"
+@outter_fun
+def f1(str1):
+    # print("f1")
+    str1 = "0001"+str1
+    return str1
 
-in1 = outter_fun()
+@outter_fun
+def f2(str1):
+    # print("f1")
+    str1 = "0002"+str1
+    return str1
+print("===================================")
+f1("Amit")
+f2("Sneha")
+'''
+
+in1 = outter_fun(f1)
 print(in1)
-retval = in1()
+retval = in1("Bhima")
 print(retval)
 
+
+r1 = f1("Bhima")
+print(r1)
+'''
