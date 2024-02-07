1085a6e2a21256f66e12215f13f61cfc9b57c730
bhimatak <bhima.t@gmail.com>
2024-02-07 16:29:18 +0530

    commit

--- Prgs/Day03_part2.py
+++ Prgs/Day03_part2.py
@@ -41,15 +41,47 @@ print(x(x(2)))
 
 '''
 
-def f1(x,y):
-    return x+y
+def f1(x):
+    if x%2 == 0:
+        return x**2
+    else:
+        return x**3
 
 l1 = [1,2,3,4,5]
 l4 = [5,6,7,8,9]
-a=10
-b=20
-l3 = list(map(f1, a,b))
+s1 = {1,2,3}
+s2 = {4,5,6}
+
+l3 = []
+for i in l1:
+    l3.append(f1(i))
 print(l3)
 
+l5 = tuple(map(f1,l1))
+print(l5, type(l5))
+def f2(str1):
+    str1 +="101"
+    return str1
+
+str2 = "ABCD"
+l6 = list(map(f2,str2))
+str4 ="".join(l6)
+print(str4)
 
 
+'''
+a=10
+b=20
+
+l3 = list(map(f1, l1))
+print(l3)
+'''
+
+def f3(x,y):
+    return x+y
+
+a = {10,20,30,101,102}
+b = {40,50,60,70,35}
+
+l6 = list(map(f3,a,b))
+print(l6)
