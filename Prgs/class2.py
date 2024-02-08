70d9f442e6d291038a881bd126c39760f160300b
bhimatak <bhima.t@gmail.com>
2024-02-08 15:11:54 +0530

    updated

--- Prgs/class2.py
+++ Prgs/class2.py
@@ -5,6 +5,11 @@ class Person:
         self.__name = name
         self.__phone = phno
 
+    def __str__(self):
+        str1 = (f'Name: {self.__name}\n'
+                f'Age: {self.__age}\n'
+                f'PhNo: {self.__phone}\n')
+        return str1
     def setAge(self, age):
         self.__age = age
     def getAge(self):
@@ -30,6 +35,7 @@ class Person:
 
 
 
+
 p1 = Person(10)
 p1.printDetails()
 print("===================")
@@ -37,3 +43,22 @@ p1.setAge(45)
 p1.setName("Bhima")
 p1.setPhNo(9980156833)
 p1.printDetails()
+
+print(p1.getPhNo())
+p2 = Person()
+p2.printDetails()
+
+
+
+'''
+define a class of rectangle
+width and height
+
+r1 = h10,w20
+r2 = h30,w40
+
+rest = r1+r2 = h40,w60
+
+
+'''
+
